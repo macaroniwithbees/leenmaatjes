7eb10a45f30832b44ed48eab5af3e3e0e177a60f
naomi <148772150+macaroniwithbees@users.noreply.github.com>
2026-05-28 10:19:24 +0200

    feat: nativewind setup & test

--- App.tsx
+++ App.tsx
@@ -1,20 +1,15 @@
+import "./global.css";
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Welkom bij Leenmaat!</Text>
-      <StatusBar style="auto" />
+    <View className="flex-1 items-center justify-center bg-white">
+      <Text className="text-xl font-bold">
+        Welkom bij Leenmaat!
+      </Text>
     </View>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+
